c1ac7a2eca550c784153ee387adbf0c73be2572f
Luiz Henrique <luizgarciaevilalva@gmail.com>
2023-02-27 21:18:49 -0300

    alterando text do socialdev

--- src/components/projetos/SocialDev.jsx
+++ src/components/projetos/SocialDev.jsx
@@ -62,10 +62,10 @@ function SocialDev() {
         <H4>Social Dev</H4>
         <StyledParagraph>
           Uma rede social para programadores, esse projeto foi feito em duas semanas, foi utilizado
-          no frontend React, NextJs e styled components, o backend foi feito com NextJs e os dados
-          dos usuarios foram enviados para ORM mongoose e armazedado no banco de dados MongoDb, com
-          sistema de login e cadastro, podendo dentro dele criar e publicar textos, editar e deletar
-          textos já publicados de sua autoria.
+          no frontend React, NextJs e styled components, o backend foi feito em NodeJs com NextJs e
+          os dados dos usuarios foram enviados para ORM mongoose e armazedado no banco de dados
+          MongoDb, com sistema de login e cadastro, podendo dentro dele criar e publicar textos,
+          editar e deletar textos já publicados de sua autoria.
         </StyledParagraph>
         <Links link={'https://social-dev-omega.vercel.app/login'} />
       </StyledContent>
